6d385e6cd0256dc7fa8839e6964f82c0b8867ba4
Imran Sultan <isultan030@gmail.com>
2019-11-07 15:40:09 -0600

    Fitting params optimization zeta=0

--- cc_optimize.py
+++ cc_optimize.py
@@ -4,19 +4,7 @@ import plot_subhalo_mass_fn
 from itk import gio_read_dict
 import numpy as np
 
-normLogCnts = False
-
-# initial fitting parameters: 2016a
-# x0 = [0.86, 0.07]
-x0 = [1, 0]
-M1, M2 = 10**14.0, 10**14.5
-r = (-6, 0)
-
-fitr = (-3, -1.5)
-
-x_shmf, y_shmf = plot_subhalo_mass_fn.plotSHMF(M1, M2, r, plotFlag=False, normLogCnts=normLogCnts)
-mask = (fitr[0]<=x_shmf)&(x_shmf<=fitr[1])
-
+normLogCnts = True
 
 def f(x):
     A, zeta = x[0], x[1]
@@ -31,25 +19,56 @@ def f(x):
 
     return (y_cmf-y_shmf)[mask]
 
-vars_cc = [
-'core_tag', 
-'tree_node_index',
-'infall_mass', 
-'infall_step', 
-'central', 
-'host_core'
-]
-cc = gio_read_dict('/home/isultan/data/AlphaQ/core_catalog_merg/09_03_2019.AQ.499.coreproperties', vars_cc)
-
 def f_zeta0(x):
     A = x
     print "A={}".format(A)
-    _, y_cmf = plot_subhalo_mass_fn.plotCMF('', M1, M2, s1=True, returnUnevolved=False, r=r, plotFlag=False, cc=cc, normLogCnts=normLogCnts, returnZeta0=True, A=A)
+    _, y_cmf = plot_subhalo_mass_fn.plotCMF('', M1, M2, s1=True, returnUnevolved=False, r=r, plotFlag=True, label='A={}'.format(A), cc=cc, normLogCnts=normLogCnts, returnZeta0=True, A=A)
 
     ans = np.linalg.norm( (y_cmf-y_shmf)[mask] )
-    print ans
+    print "error={}%".format( ans/np.linalg.norm(y_shmf[mask]) * 100. )
     return ans
 
 if __name__ == "__main__":
+    # initial fitting parameters: 2016a
+    # x0 = [0.86, 0.07]
+    # x0 = [1, 0]
+    plot_subhalo_mass_fn.plt.figure(dpi=120)
+    M1, M2 = 10**14.0, 10**14.5
+    r = (-6, 0)
+
+    fitr = (-3, -1.5)
+
+    x_shmf, y_shmf = plot_subhalo_mass_fn.plotSHMF(M1, M2, r, plotFlag=True, normLogCnts=normLogCnts)
+    mask = (fitr[0]<=x_shmf)&(x_shmf<=fitr[1])
+    vars_cc = [
+        'core_tag', 
+        'tree_node_index',
+        'infall_mass', 
+        'infall_step', 
+        'central', 
+        'host_core'
+    ]
+    cc = gio_read_dict('/home/isultan/data/AlphaQ/core_catalog_merg/09_03_2019.AQ.499.coreproperties', vars_cc)
     # least_squares(f, x0, verbose=2)
-    minimize_scalar(f_zeta0, bounds=(.1, 10), method='bounded')
+    # print minimize_scalar(f_zeta0, bounds=(.1, 10), method='bounded')
+    # plot_subhalo_mass_fn.plt.legend()
+    
+    # plot_subhalo_mass_fn.plt.figure(dpi=120)
+    # xarr = np.arange(.1, 10, .1)
+    # yarr = [f_zeta0(x) for x in xarr]
+    # plot_subhalo_mass_fn.plt.figure(dpi=120)
+    # plot_subhalo_mass_fn.plt.plot(xarr, yarr)
+    # plot_subhalo_mass_fn.plt.xlabel('A')
+    # plot_subhalo_mass_fn.plt.ylabel('$|residual|$')
+
+    plot_subhalo_mass_fn.plt.figure(dpi=120)
+    xarr = np.arange(.5, 1.5, .01)
+    yarr = [f_zeta0(x) for x in xarr]
+    plot_subhalo_mass_fn.plt.figure(dpi=120)
+    plot_subhalo_mass_fn.plt.plot(xarr, yarr)
+    plot_subhalo_mass_fn.plt.xlabel('A')
+    plot_subhalo_mass_fn.plt.ylabel('$|residual|$')
+    
+    
+    plot_subhalo_mass_fn.plt.show()
+    
